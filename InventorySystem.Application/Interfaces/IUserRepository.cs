b46ee11a153e2bdc305fa92ceba1752d9ce78eb1
uwunish <anishkoirala2001@gmail.com>
2026-06-06 19:19:57 +0545

    Create IUserRepository

---
+++ InventorySystem.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using InventorySystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByIdAsync(int id);
+        Task<User?> GetByEmailAsync(string email);
+        Task<IEnumerable<User>> GetAllUsersAsync();
+        Task<User> CreateAsync(User user);
+        Task<User> UpdateAsync(User user);
+        Task<bool> EmailExistsAsync(string email);
+    }
+}
