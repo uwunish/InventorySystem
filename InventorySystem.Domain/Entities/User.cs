c1e796a6aeb51c7a17923d0c5ec953a0e53da072
uwunish <anishkoirala2001@gmail.com>
2026-06-06 09:38:13 +0545

    Created a User entity

---
+++ InventorySystem.Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using InventorySystem.Domain.Common;
+using InventorySystem.Domain.Enums;
+
+namespace InventorySystem.Domain.Entities
+{
+	public class User : BaseEntity
+	{
+		public string Name { get; set; } = string.Empty;
+		public string Email { get; set; } = string.Empty;
+		public string MobileNumber { get; set; } = string.Empty;
+		public UserStatus Status { get; set; } = UserStatus.Active;
+		public string PasswordHash { get; set; } = string.Empty;
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+	}
+}
