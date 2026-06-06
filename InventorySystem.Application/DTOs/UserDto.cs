bdae74fb1ce826416e8c4ea9398052e02fc574eb
uwunish <anishkoirala2001@gmail.com>
2026-06-06 19:15:01 +0545

    Create a User Dto

--- InventorySystem.Application/DTOs/UserDto.cs
+++ InventorySystem.Application/DTOs/UserDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InventorySystem.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,11 @@ namespace InventorySystem.Application.DTOs
 {
     public class UserDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
+        public Status Status { get; set; }
+        public DateTime CreatedAt { get; set; }
     }
 }
