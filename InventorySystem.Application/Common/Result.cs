74e9c705627a94abd071f34e76a7f824fdb90f54
uwunish <anishkoirala2001@gmail.com>
2026-06-06 19:13:19 +0545

    Create a Result class which gives us structured custom api response

---
+++ InventorySystem.Application/Common/Result.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Common
+{
+    public class Result<T>
+    {
+        public bool Succeeded { get; private set; }
+        public T? Data { get; private set; }
+        public string? Error { get; private set; }
+
+        private Result() { }
+
+        public static Result<T> Success(T data) =>
+            new Result<T> { Succeeded = true, Data = data };
+
+        public static Result<T> Failure(string error) =>
+            new Result<T> { Succeeded = false, Error = error };
+    }
+}
---
+++ InventorySystem.Application/DTOs/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.DTOs
+{
+    public class UserDto
+    {
+    }
+}
