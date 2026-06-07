c19bb55b7df435b77e5f7468b75a0035c5376de0
uwunish <anishkoirala2001@gmail.com>
2026-06-07 19:56:31 +0545

    Add GetUsersQuery

--- InventorySystem.Application/Features/Users/Queries/GetUsersQuery.cs
+++ InventorySystem.Application/Features/Users/Queries/GetUsersQuery.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using InventorySystem.Application.Common;
+using InventorySystem.Application.DTOs;
+using InventorySystem.Application.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace InventorySystem.Application.Features.Users.Queries
 {
-    internal class GetUsersQuery
+    public class GetUsersQuery
     {
+    }
+
+    public class GetUsersQueryHandler
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUsersQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<Result<IEnumerable<UserDto>>> Handle(GetUsersQuery query)
+        {
+            var users = await _userRepository.GetAllUsersAsync();
+
+            var userDtos = users.Select(x => new UserDto
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Email = x.Email,
+                MobileNumber = x.MobileNumber,
+                Status = x.Status,
+                CreatedAt = x.CreatedAt
+            });
+
+            return Result<IEnumerable<UserDto>>.Success(userDtos);
+        }
     }
 }
