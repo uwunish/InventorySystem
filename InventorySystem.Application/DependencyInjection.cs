135e119571a8a77b95710ab77f6f0e97f4e6446a
uwunish <anishkoirala2001@gmail.com>
2026-06-08 22:08:04 +0545

    add dependency injection class inside Application project

---
+++ InventorySystem.Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using InventorySystem.Application.Features.Auth.Commands;
+using InventorySystem.Application.Features.Users.Commands;
+using InventorySystem.Application.Features.Users.Queries;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<CreateUserCommandHandler>();
+            services.AddScoped<UpdateUserCommandHandler>();
+            services.AddScoped<LoginCommandHandler>();
+
+            services.AddScoped<GetUsersQueryHandler>();
+            services.AddScoped<GetUserByIdQueryHandler>();
+
+            return services;
+        }
+    }
+}
