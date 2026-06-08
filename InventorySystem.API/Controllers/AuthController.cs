788a6baade845ccfadba6305906a260d5758c246
uwunish <anishkoirala2001@gmail.com>
2026-06-08 22:24:44 +0545

    add AuthController

---
+++ InventorySystem.API/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using InventorySystem.Application.Features.Auth.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InventorySystem.API.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly LoginCommandHandler _loginHandler;
+
+        public AuthController(LoginCommandHandler loginHandler)
+        {
+            _loginHandler = loginHandler;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginCommand command)
+        {
+            var result = await _loginHandler.Handle(command);
+
+            if (!result.Succeeded)
+                return Unauthorized(new { message = result.Error });
+
+            return Ok(result.Data);
+        }
+    }
+}
