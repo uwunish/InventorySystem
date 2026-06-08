5ff9625dcc17671ed875ebee7f05a86c64e105f5
uwunish <anishkoirala2001@gmail.com>
2026-06-08 21:08:30 +0545

    validate token when user request the api for something

--- InventorySystem.Infrastructure/DependencyInjection.cs
+++ InventorySystem.Infrastructure/DependencyInjection.cs
@@ -1,7 +1,12 @@
-﻿using InventorySystem.Infrastructure.Persistence;
+﻿using InventorySystem.Application.Interfaces;
+using InventorySystem.Infrastructure.Persistence;
+using InventorySystem.Infrastructure.Repositories;
+using InventorySystem.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -15,11 +20,38 @@ namespace InventorySystem.Infrastructure
             IConfiguration configuration
             )
         {
+            // database
             services.AddDbContext<AppDbContext>(options =>
             options.UseSqlServer(
                 configuration.GetConnectionString("DefaultConnection")
                 )
             );
+
+            // repositories
+            services.AddScoped<IUserRepository, UserRepository>();
+
+            // services
+            services.AddScoped<IAuthService, AuthService>();
+
+            // jwt
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            var secretKey = jwtSettings["SecretKey"]!;
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings["Issuer"],
+                        ValidAudience = jwtSettings["Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey))
+                    };
+                });
+
             return services;
         }
     }
