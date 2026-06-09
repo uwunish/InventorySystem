45a877839275ad370afa77eca5ec080b871f2687
uwunish <anishkoirala2001@gmail.com>
2026-06-09 20:29:21 +0545

    create dto for Unit of measure

---
+++ InventorySystem.Application/DTOs/UnitOfMeasureDto.cs
@@ -0,0 +1,18 @@
+﻿using InventorySystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.DTOs
+{
+    public class UnitOfMeasureDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public Status Status { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
