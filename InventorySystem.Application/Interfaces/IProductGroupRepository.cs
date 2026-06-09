ef2def89401dfcd490ff0129df6ff0f9c358b2d7
uwunish <anishkoirala2001@gmail.com>
2026-06-09 20:18:06 +0545

    create interface for product group repository

---
+++ InventorySystem.Application/Interfaces/IProductGroupRepository.cs
@@ -0,0 +1,16 @@
+﻿using InventorySystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Interfaces
+{
+    public interface IProductGroupRepository
+    {
+        Task<ProductGroup> GetByIdAsync(int id);
+        Task<IEnumerable<ProductGroup>> GetAllAsync();
+        Task<ProductGroup> CreateAsync(ProductGroup productGroup);
+        Task<ProductGroup> UpdateAsync(ProductGroup productGroup);
+        Task<bool> NameExistsAsync(string name, int? excludeId = null);
+    }
+}
