61d96466ae1e1962fe021a85aa775b7d916ab576
uwunish <anishkoirala2001@gmail.com>
2026-06-09 21:24:51 +0545

    add routes to product, unit of measure and product group

--- InventorySystemFrontend/src/app/app.routes.ts
+++ InventorySystemFrontend/src/app/app.routes.ts
@@ -8,6 +8,63 @@ export const routes: Routes = [
         .then(m => m.LoginComponent)
   },
   {
+    path: 'product-groups', canActivate: [authGuard], children: [
+      {
+        path: '', loadComponent: () =>
+          import('./pages/product-groups/product-group-list/product-group-list.component')
+            .then(m => m.ProductGroupListComponent)
+      },
+      {
+        path: 'new', loadComponent: () =>
+          import('./pages/product-groups/product-group-form/product-group-form.component')
+            .then(m => m.ProductGroupFormComponent)
+      },
+      {
+        path: 'edit/:id', loadComponent: () =>
+          import('./pages/product-groups/product-group-form/product-group-form.component')
+            .then(m => m.ProductGroupFormComponent)
+      }
+    ]
+  },
+  {
+    path: 'unit-of-measures', canActivate: [authGuard], children: [
+      {
+        path: '', loadComponent: () =>
+          import('./pages/unit-of-measures/uom-list/uom-list.component')
+            .then(m => m.UomListComponent)
+      },
+      {
+        path: 'new', loadComponent: () =>
+          import('./pages/unit-of-measures/uom-form/uom-form.component')
+            .then(m => m.UomFormComponent)
+      },
+      {
+        path: 'edit/:id', loadComponent: () =>
+          import('./pages/unit-of-measures/uom-form/uom-form.component')
+            .then(m => m.UomFormComponent)
+      }
+    ]
+  },
+  {
+    path: 'products', canActivate: [authGuard], children: [
+      {
+        path: '', loadComponent: () =>
+          import('./pages/products/product-list/product-list.component')
+            .then(m => m.ProductListComponent)
+      },
+      {
+        path: 'new', loadComponent: () =>
+          import('./pages/products/product-form/product-form.component')
+            .then(m => m.ProductFormComponent)
+      },
+      {
+        path: 'edit/:id', loadComponent: () =>
+          import('./pages/products/product-form/product-form.component')
+            .then(m => m.ProductFormComponent)
+      }
+    ]
+  },
+  {
     path: 'users', loadComponent: () =>
       import('./pages/users/user-list/user-list.component')
         .then(m => m.UserListComponent),
