ef30efb1c5cda53df7505ea92a16b15fa1fcadea
uwunish <anishkoirala2001@gmail.com>
2026-06-09 20:46:16 +0545

    register handlers for product, unit of measure and product group

--- InventorySystem.Application/DependencyInjection.cs
+++ InventorySystem.Application/DependencyInjection.cs
@@ -1,4 +1,10 @@
 ﻿using InventorySystem.Application.Features.Auth.Commands;
+using InventorySystem.Application.Features.ProductGroups.Commands;
+using InventorySystem.Application.Features.ProductGroups.Queries;
+using InventorySystem.Application.Features.Products.Commands;
+using InventorySystem.Application.Features.Products.Queries;
+using InventorySystem.Application.Features.UnitOfMeasures.Commands;
+using InventorySystem.Application.Features.UnitOfMeasures.Queries;
 using InventorySystem.Application.Features.Users.Commands;
 using InventorySystem.Application.Features.Users.Queries;
 using Microsoft.Extensions.DependencyInjection;
@@ -12,12 +18,32 @@ namespace InventorySystem.Application
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
-            services.AddScoped<CreateUserCommandHandler>();
-            services.AddScoped<UpdateUserCommandHandler>();
+            // auth
             services.AddScoped<LoginCommandHandler>();
 
+            // users
+            services.AddScoped<CreateUserCommandHandler>();
+            services.AddScoped<UpdateUserCommandHandler>();
             services.AddScoped<GetUsersQueryHandler>();
             services.AddScoped<GetUserByIdQueryHandler>();
+
+            // product groups
+            services.AddScoped<CreateProductGroupCommandHandler>();
+            services.AddScoped<UpdateProductGroupCommandHandler>();
+            services.AddScoped<GetProductGroupsQueryHandler>();
+            services.AddScoped<GetProductGroupByIdQueryHandler>();
+
+            // unit of measures
+            services.AddScoped<CreateUnitOfMeasureCommandHandler>();
+            services.AddScoped<UpdateUnitOfMeasureCommandHandler>();
+            services.AddScoped<GetUnitOfMeasuresQueryHandler>();
+            services.AddScoped<GetUnitOfMeasureByIdQueryHandler>();
+
+            // products
+            services.AddScoped<CreateProductCommandHandler>();
+            services.AddScoped<UpdateProductCommandHandler>();
+            services.AddScoped<GetProductsQueryHandler>();
+            services.AddScoped<GetProductByIdQueryHandler>();
 
             return services;
         }
