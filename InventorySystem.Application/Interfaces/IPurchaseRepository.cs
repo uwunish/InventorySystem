14e30fe07831cea1c957191e72e2de8d648eb3c5
uwunish <anishkoirala2001@gmail.com>
2026-06-10 11:02:21 +0545

    create interface for purchase repository

---
+++ InventorySystem.Application/Interfaces/IPurchaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using InventorySystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Interfaces
+{
+    public interface IPurchaseRepository
+    {
+        Task<Purchase?> GetByIdAsync(int id);
+        Task<IEnumerable<Purchase>> GetAllAsync();
+        Task<Purchase> CreateAsync(Purchase purchase);
+    }
+}
