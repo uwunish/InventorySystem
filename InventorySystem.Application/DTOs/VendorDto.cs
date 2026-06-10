ec7e2340f49a5dd3c5c4ac717bf7b6cf3cfd6cfe
uwunish <anishkoirala2001@gmail.com>
2026-06-10 10:52:01 +0545

    create vendor dto

---
+++ InventorySystem.Application/DTOs/VendorDto.cs
@@ -0,0 +1,17 @@
+﻿using InventorySystem.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.DTOs
+{
+    public class VendorDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Descripition { get; set; } = string.Empty;
+        public Status Status { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
