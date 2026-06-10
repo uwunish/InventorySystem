48b36b0212ae86106da431ffae3f4080d53a3148
uwunish <anishkoirala2001@gmail.com>
2026-06-10 11:44:28 +0545

    create component for purchase detail and purchase list

--- InventorySystemFrontend/src/app/pages/purchases/purchase-detail/purchase-detail.component.ts
+++ InventorySystemFrontend/src/app/pages/purchases/purchase-detail/purchase-detail.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { PurchaseService } from '../../../services/purchase.service';
+import { PurchaseDto } from '../../../models/purchase.model';
 
 @Component({
   selector: 'app-purchase-detail',
-  imports: [],
-  templateUrl: './purchase-detail.html',
-  styleUrl: './purchase-detail.scss',
+  standalone: true,
+  imports: [CommonModule, MatCardModule, MatTableModule,
+    MatButtonModule, MatIconModule, MatDividerModule],
+  templateUrl: './purchase-detail.component.html',
+  styleUrls: ['./purchase-detail.component.scss']
 })
-export class PurchaseDetail {}
+export class PurchaseDetailComponent implements OnInit {
+  purchase?: PurchaseDto;
+  displayedColumns = [
+    'productName', 'quantity', 'purchasePrice', 'salesPrice', 'lineTotal'
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private purchaseService: PurchaseService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const id = +this.route.snapshot.params['id'];
+    this.purchaseService.getById(id).subscribe({
+      next: (data) => this.purchase = data,
+      error: () => this.router.navigate(['/purchases'])
+    });
+  }
+
+  goBack(): void {
+    this.router.navigate(['/purchases']);
+  }
+}
--- InventorySystemFrontend/src/app/pages/purchases/purchase-list/purchase-list.component.ts
+++ InventorySystemFrontend/src/app/pages/purchases/purchase-list/purchase-list.component.ts
@@ -1,9 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { PurchaseService } from '../../../services/purchase.service';
+import { PurchaseDto } from '../../../models/purchase.model';
 
 @Component({
   selector: 'app-purchase-list',
-  imports: [],
-  templateUrl: './purchase-list.html',
-  styleUrl: './purchase-list.scss',
+  standalone: true,
+  imports: [CommonModule, MatTableModule, MatButtonModule, MatIconModule],
+  templateUrl: './purchase-list.component.html',
+  styleUrls: ['./purchase-list.component.scss']
 })
-export class PurchaseList {}
+export class PurchaseListComponent implements OnInit {
+  purchases: PurchaseDto[] = [];
+  displayedColumns = [
+    'id', 'purchaseDate', 'vendorName',
+    'totalAmount', 'createdByName', 'actions'
+  ];
+  isLoading = false;
+
+  constructor(
+    private purchaseService: PurchaseService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.purchaseService.getAll().subscribe({
+      next: (data) => {
+        this.purchases = data;
+        this.isLoading = false;
+      },
+      error: () => this.isLoading = false
+    });
+  }
+
+  viewDetail(id: number): void {
+    this.router.navigate(['/purchases', id]);
+  }
+
+  newPurchase(): void {
+    this.router.navigate(['/purchases/new']);
+  }
+}
