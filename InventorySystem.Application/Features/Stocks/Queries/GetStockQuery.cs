e8256c6ac619bcd682cd1cbba86389db361b8ea1
uwunish <anishkoirala2001@gmail.com>
2026-06-10 12:22:54 +0545

    create stock query

---
+++ InventorySystem.Application/Features/Stocks/Queries/GetStockQuery.cs
@@ -0,0 +1,28 @@
+﻿using InventorySystem.Application.Common;
+using InventorySystem.Application.DTOs;
+using InventorySystem.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Features.Stocks.Queries
+{
+    public class GetCurrentStockQuery { }
+
+    public class GetCurrentStockQueryHandler
+    {
+        private readonly IStockRepository _repository;
+
+        public GetCurrentStockQueryHandler(IStockRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result<IEnumerable<StockItemDto>>> Handle(
+            GetCurrentStockQuery query)
+        {
+            var stock = await _repository.GetCurrentStockAsync();
+            return Result<IEnumerable<StockItemDto>>.Success(stock);
+        }
+    }
+}
