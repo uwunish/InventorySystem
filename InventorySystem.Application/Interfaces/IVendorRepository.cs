878a318e80c964fad1ff16de120e248cae499cf1
uwunish <anishkoirala2001@gmail.com>
2026-06-10 10:53:18 +0545

    create interface for vendor repository

---
+++ InventorySystem.Application/Interfaces/IVendorRepository.cs
@@ -0,0 +1,16 @@
+﻿using InventorySystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Interfaces
+{
+    public interface IVendorRepository
+    {
+        Task<Vendor?> GetByIdAsync(int id);
+        Task<IEnumerable<Vendor>> GetAllAsync();
+        Task<Vendor> CreateAsync(Vendor vendor);
+        Task<Vendor> UpdateAsync(Vendor vendor);
+        Task<bool> NameExistsAsync(string name, int? excludeId = null);
+    }
+}
