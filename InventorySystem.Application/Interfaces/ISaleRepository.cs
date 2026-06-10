d31cd0c7137cf87b7be2fc7bd96ba9da30a4f963
uwunish <anishkoirala2001@gmail.com>
2026-06-10 12:18:06 +0545

    create interfaces for sale and stock

---
+++ InventorySystem.Application/Interfaces/ISaleRepository.cs
@@ -0,0 +1,14 @@
+﻿using InventorySystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Interfaces
+{
+    public interface ISaleRepository
+    {
+        Task<Sale?> GetByIdAsync(int id);
+        Task<IEnumerable<Sale>> GetAllAsync();
+        Task<Sale> CreateAsync(Sale sale);
+    }
+}
---
+++ InventorySystem.Application/Interfaces/IStockRepository.cs
@@ -0,0 +1,13 @@
+﻿using InventorySystem.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Interfaces
+{
+    public interface IStockRepository
+    {
+        Task<IEnumerable<StockItemDto>> GetCurrentStockAsync();
+        Task<StockItemDto?> GetStockForProductAsync(int productId);
+    }
+}
