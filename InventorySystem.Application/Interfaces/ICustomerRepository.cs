0f535eae472f37f3e5c82b1dc1b93e25de915054
uwunish <anishkoirala2001@gmail.com>
2026-06-10 11:16:37 +0545

    create interface for customer repository

---
+++ InventorySystem.Application/Interfaces/ICustomerRepository.cs
@@ -0,0 +1,16 @@
+﻿using InventorySystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventorySystem.Application.Interfaces
+{
+    public interface ICustomerRepository
+    {
+        Task<Customer?> GetByIdAsync(int id);
+        Task<IEnumerable<Customer>> GetAllAsync();
+        Task<Customer> CreateAsync(Customer customer);
+        Task<Customer> UpdateAsync(Customer customer);
+        Task<bool> NameExistsAsync(string name, int? excludeId = null);
+    }
+}
