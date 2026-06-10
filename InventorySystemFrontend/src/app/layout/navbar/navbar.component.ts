1c9a58e3e0d0caa5dfc74b9f9d5ab708a1a92389
uwunish <anishkoirala2001@gmail.com>
2026-06-10 16:18:14 +0545

    update navbar

--- InventorySystemFrontend/src/app/layout/navbar/navbar.component.ts
+++ InventorySystemFrontend/src/app/layout/navbar/navbar.component.ts
@@ -1,18 +1,20 @@
 import { Component, inject } from '@angular/core';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { filter } from 'rxjs/operators';
 import { AuthService } from '../../services/auth.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [CommonModule, RouterModule, MatToolbarModule,
     MatButtonModule, MatIconModule, MatMenuModule],
-  templateUrl: './navbar.component.html'
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
   private authService = inject(AuthService);
